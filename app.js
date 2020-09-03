f9445cc69946c37d23b7dd20598c46cd87c7f3c7
miyamoto <myumyu112112@gmail.com>
2020-09-03 12:05:59 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const memo = new Map();
 memo.set(0, 0);
 memo.set(1, 1);
 function fib(n) {
-  if(memo.has(n)) {
+  if (memo.has(n)){
     return memo.get(n);
   }
-  const value = fib(n - 1) + fib(n - 2);
+  const value = fib(n-1) + fib(n - 2);
   memo.set(n, value);
   return value;
 }
